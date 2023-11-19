fc3b443e44da481456f62d0a73717bc35c452d57
Dali Wang <daliwang@users.noreply.github.com>
2023-11-19 09:46:52 -0500

    Add args in main

--- AOIcreation.py
+++ AOIcreation.py
@@ -4,12 +4,19 @@ from pyproj import Transformer
 import matplotlib.pyplot as plt
 from scipy.spatial import cKDTree
 import pandas as pd
-import sys
+import sys, os
+
+from datetime import datetime
+
+# Get current date
+current_date = datetime.now()
+# Format date to mmddyyyy
+formatted_date = current_date.strftime('%m%d%Y')
 
 # An input csv file that contains the locations of land gridcells in Daymet domain;
 # 1:  land gridcell ID
-# 2:  lat, lon of land gridcell center 
-# 3:  Y,X location in LLC projection
+# 2:  xc, yc (lon, lat) of land gridcell center 
+# 3:  xc_LCC,yc_LCC (x,y) location in LLC projection
 
 # output 1D AOIdomain.nc that can be used to generate 1D AOIsurfdata.nc and 1D AOIforcing.nc 
 
@@ -26,6 +33,15 @@ def find_nearest_points(listA, listB):
     return indices
 
 def main():
+
+    # Check the number of arguments
+    if len(sys.argv) != 4  or sys.argv[1] == '--help':  # sys.argv includes the script name as the first argument
+        print("Example use: python AOIcreation.py <points_file_name> <AOI_domain_name>")
+        print(" <points_file_name>:  <domain>_gridID.csv, <domain>_xcyc.csv, <domain>_xcyc_LCC.csv")
+        print(" <user_option>:  1-> gridID, 2-> xcyc, 3-> xcyc_LCC")
+        print(" <AOI_domain>:  Uniqe string for AOI domain name")
+        exit(0)
+
     args = sys.argv[1:]
     AOI_gridcell_file = args[0]  # user provided gridcell csv file
     user_option = args[1]
@@ -43,15 +59,12 @@ def main():
 
     # open the 1D domain data
     src = nc.Dataset(source_file, 'r', format='NETCDF4')
-    # read yc_llc, xc_llc (y, x in LLC)
-    NA_yc_LCC = src.variables['yc_LCC'][:]
-    NA_xc_LCC = src.variables['xc_LCC'][:]
 
     # 3) Open a csv file to read a list of points (y, x)
     #df = read_gridcells(AOI_gridcell_file)  
 
     if user_option == '1': # gridID is used directly
-        AOI_gridcell_file = 'AKSPgridIDs.csv'  # user provided gridcell IDs
+        #AOI_gridcell_file = AOI+'_gridID.csv'  # user provided gridcell IDs
         df = pd.read_csv(AOI_gridcell_file, sep=",", skiprows=1, names = ['gridID'])
         #read gridIds
         AOI_points = list(df['gridID'])
@@ -65,13 +78,16 @@ def main():
         domain_idx = np.where(np.in1d(NA_gridcell_list, AOI_points))[0]
 
     if user_option == '2': # use lat lon coordinates
-        AOI_gridcell_file = 'AKSP_xcyc.csv'  # user provided gridcell csv file  (xc, yc) (lon, lat)
+        #AOI_gridcell_file = AOI+'_xcyc.csv'  # user provided gridcell csv file  (xc, yc) (lon, lat)
         df = pd.read_csv(AOI_gridcell_file, sep=",", skiprows=1, names = ['xc', 'yc'], engine='python')
 
         #read in x, y coordinate (lon, lat)
         AOI_points = list(zip(df['xc'], df['yc']))
         #AOI_points = list(zip(myxc, myyc))
         
+        # read yc, xc (y, x)
+        NA_yc = src.variables['yc'][:]
+        NA_xc = src.variables['xc'][:]
         # create list for all land gridcell (lat, lon)
         NA_gridcell_list = list(zip(NA_xc, NA_yc))
 
@@ -83,32 +99,33 @@ def main():
         tree = cKDTree(NA_gridcell_arr)
         _, domain_idx = tree.query(AOI_points_arr, k=1)
 
-    if user_option == '3': # xc_LLC and yc_LLC is used directly
-        AOI_gridcell_file = 'AKSP_XYLLC.csv'  # user provided gridcell csv file  (xc_LLC, yc_LLC) 
-        df = pd.read_csv(AOI_gridcell_file, sep=",", skiprows=1, names = ['xc_LLC', 'yc_LLC'], engine='python')
+    if user_option == '3': # xc_LCC and yc_LCC is used directly
+        #AOI_gridcell_file = AOI+'_XYLCC.csv'  # user provided gridcell csv file  (xc_LLC, yc_LLC) 
+        df = pd.read_csv(AOI_gridcell_file, sep=",", skiprows=1, names = ['xc_LCC', 'yc_LCC'], engine='python')
 
         #read in x, y coordinate (in LLC projection)
-        AOI_points = list(zip(df['xc_LLC'], df['yc_LLC']))
+        AOI_points = list(zip(df['xc_LCC'], df['yc_LCC']))
         #AOI_points = list(zip(myxc_lcc, myyc_lcc))
+
+        # read yc_LCC, xc_LCC (y, x in LCC)
+        NA_yc_LCC = src.variables['yc_LCC'][:]
+        NA_xc_LCC = src.variables['xc_LCC'][:]
+
         # create list for all land gridcell (lat, lon)
-        NA_gridcell_list = list(zip(NA_xc_LLC, NA_yc_LLC))
+        NA_gridcell_list = list(zip(NA_xc_LCC, NA_yc_LCC))
 
         AOI_points_arr = np.array(AOI_points)
         print("AOI_points_arr", AOI_points_arr[0:5], "shape", AOI_points_arr.shape)
         NA_gridcell_arr = np.squeeze(np.array(NA_gridcell_list)).transpose()    
         print("NA_gridcell_arr", NA_gridcell_arr[0:5], "shape", NA_gridcell_arr.shape)
 
-    #Find the nearest points in the 1D list of points and return the index of these nearest points
-    #landcells = NA_yc.shape[1]
-    #NA_gridcell_arr = np.array(NA_gridcell_list).reshape(2, landcells).transpose()  #array (n,m) n data points of m dimensions
-    #AOI_points_arr = np.array(AOI_points).transpose()
-
+  
         tree = cKDTree(NA_gridcell_arr)
         _, domain_idx = tree.query(AOI_points_arr, k=1)
 
     domain_idx = np.sort(domain_idx).squeeze()
     print("gridID_idx", domain_idx[0:10])
-    np.savetxt("AOIgridId_idx.csv", domain_idx[0:100], delimiter=",", fmt='%d')
+    #np.savetxt("AOIgridId_idx.csv", domain_idx[0:100], delimiter=",", fmt='%d')
 
     # Copy the global attributes from the source to the target
     for name in src.ncattrs():
