7b61f25778a28c902d4157b7314f5fcead1cd8e1
Dali Wang <daliwang@users.noreply.github.com>
2023-11-23 11:05:44 -0500

    add AOI_forcing and rename AOI_domainGEN.py

--- AOI_forcingGEN.py
+++ AOI_forcingGEN.py
@@ -32,26 +32,24 @@ def AOI_forcing_save_1d(input_path, file, AOI, AOI_points, var_name, period, tim
 
     # read forcing data
     data = src[var_name][0:time, :] # read (time, gridcell) format
-
+    print(data.shape)
     # create the mask for data subsetting using AOI points
     # TODO
     #AOI_mask = np.where(~np.isnan(mask), 1, np.nan)
     #AOI_landcells = len(AOI_points)
 
     # Create a boolean mask
-    AOI_mask = np.isin(grid_ids, AOI)
-
+    AOI_mask = np.isin(grid_ids, AOI_points)
 
     # You can apply the same mask to this array
-    masked_data = data[:, mask]
+    masked_data = data[:, AOI_mask]
+
     # extract the data over land gridcells into a new data_arr for netcdf  
     data_arr = np.copy(masked_data)
-
+    print(masked_data.shape)
     # convert local grid_id_lists into an array
     #grid_id_arr = np.array(grid_ids)
 
-    #data_arr = np.array(FSDS_list[i])
-    data_arr = np.array(data)
     dst_name = output_path + '/'+ AOI + '_clmforc.Daymet4.1km.1d.' + var_name + '.' + period +'.nc'
 
     # Open a new NetCDF file to write the data to. For format, you can choose from
@@ -72,7 +70,7 @@ def AOI_forcing_save_1d(input_path, file, AOI, AOI_points, var_name, period, tim
     # Copy the variables from the source to the target
     for name, variable in src.variables.items():
         if (name == var_name):
-            print(varibale.datetype)
+            print(variable.datatype)
             w_nc_var = dst.createVariable(var_name, np.float32, ('time', 'gridcell'))
             dst.variables[var_name][:] =data_arr
             for attr_name in variable.ncattrs():
@@ -99,26 +97,27 @@ def main():
     if len(sys.argv) != 4  or sys.argv[1] == '--help':  # sys.argv includes the script name as the first argument
         print("Example use: python AOI_forcingGEN.py <points_file_name> <AOI_domain_name>")
         print(" <input_path>: path to the 1D source data directory")
-        print(" <points_file_name>:  <AOI>_gridID.csv")
         print(" <output_path>:  path for the 1D AOI forcing data directory")
+        print(" <points_file_name>:  <AOI>_gridID.csv")
         exit(0)
 
     input_path = args[0]
     output_path = args[1]
     AOI_gridID_file = args[2]
-    
+    AOI='AKSP'
     time = 1
 
     files_nc = get_files(input_path)
 
     #AOI_gridcell_file = AOI+'_gridID.csv'  # user provided gridcell IDs
-    df = pd.read_csv(AOI_gridcell_file, sep=",", skiprows=1, names = ['gridID'])
+    df = pd.read_csv(AOI_gridID_file, sep=",", skiprows=1, names = ['gridID'])
     #read gridIds
     AOI_points = np.array(df['gridID'])
 
     for f in files_nc: 
-        var_name = f[20:-11]
+        var_name = f[23:-11]
         period = f[-10:-3]
+        print(var_name, period)
         print('processing '+ var_name + '(' + period + ') in the file ' + f )
         start = process_time() 
         #forcing_save_1dNA(input_path, f, var_name, period, time, output_path)
